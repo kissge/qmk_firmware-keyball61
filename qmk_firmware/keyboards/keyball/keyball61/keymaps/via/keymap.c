f3b0cddbabae71baac37fc2cc8b38f62f2ee2117
Yusuke Kido <mail@yusuk.eki.do>
2023-05-09 08:43:13 +0900

    F13, F14を追加

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
     _______  , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_DEL   ,
     _______  , NEW_TAB  , CLOSE_TAB, PREV_TAB , NEXT_TAB , CLOSE_WIN,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , BACK     , FORWARD  ,
-    _______  ,REOPEN_TAB, _______  , KC_MUTE  , KC_VOLD  , KC_VOLU  , RESET    ,            EEP_RST  , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , _______  , _______  ,
+    _______  ,REOPEN_TAB, _______  , KC_MUTE  , KC_VOLD  , KC_VOLU  , RESET    ,            EEP_RST  , KC_HOME  , KC_PGDN  , KC_PGUP  , KC_END   , KC_F13   , KC_F14   ,
     _______  , _______  , _______  , _______  , _______  , _______  , CHANGE_OS,            KC_BTN2  , IME      , _______  , _______  , _______  , _______  , RESET
   ),
 };
